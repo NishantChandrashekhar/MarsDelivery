b2d5123763da90bc65ecee9b7e7f6418c7f66eca
NishantChandrashekhar <53821078+NishantChandrashekhar@users.noreply.github.com>
2023-08-04 10:43:34 +0530

    Lecture 51 Particles for rocket boosters

--- Assets/Scripts/RocketCollision.cs
+++ Assets/Scripts/RocketCollision.cs
@@ -5,6 +5,8 @@ public class RocketCollision : MonoBehaviour
 {
     [SerializeField] AudioClip collisionSound;
     [SerializeField] AudioClip finishSound;
+    [SerializeField] ParticleSystem explosion;
+    [SerializeField] ParticleSystem finish;
 
     AudioSource audioSource;
 
@@ -37,17 +39,37 @@ public class RocketCollision : MonoBehaviour
         }
     }
     void StartCrashSequence(){
+        // Marking the collide
         collided = true;
+
+        // Disabling the rocket movement
         GetComponent<RocketController>().enabled = false;
+        
+        // Playing audio
         audioSource.clip = collisionSound;
         if (!audioSource.isPlaying) { audioSource.Play(); }
+
+        // Triggering the explosion particles
+        explosion.Play();
+
+        // Invoking the spawn
         Invoke(nameof(ReloadLevel), delayToLoadScene);
     }
     void StartNextLevelSequence(){
+        // Marking the collide
         collided = true;
+
+        // Playing the audio
         audioSource.clip = finishSound;
         if (!audioSource.isPlaying) { audioSource.Play(); }
+
+        // Disabling the rocket movement
         GetComponent<RocketController>().enabled = false;
+
+        // Triggering the finish particles
+        finish.Play();
+
+        // Invoking the next level
         Invoke(nameof(LoadNextLevel), delayToLoadScene);
     }
     void ReloadLevel(){
